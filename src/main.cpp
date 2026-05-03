b4bb28b9592a0e7bf2ce500222eec122fc7351d5
limburger <nathwee.lim@gmail.com>
2026-05-03 01:06:27 -0400

    implement string-based argument flags (#184)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
 
 #include "Sim.h"
 
@@ -12,7 +14,6 @@ void print_usage() {
     std::cout << "\t-m N: Set the initial number of products per machine to N." << std::endl;
     std::cout << "\t-r N: Set the initial number of producers to N." << std::endl;
     std::cout << "\t-d N: Set the initial number of distributors to N." << std::endl;
-    std::cout << "\t-e N: Set the random seed to N." << std::endl;
     std::cout << "\t-s N: Set the annual chance of an agent getting sick." << std::endl;
     std::cout << "\t-j: Write JSON log traces to stdout." << std::endl;
 }
@@ -20,102 +21,67 @@ void print_usage() {
 void set_params(int argc, const char ** argv, SimArgs& args) {
     bool error = false;
     for (int i = 1; i < argc; ++i) {
-        unsigned j = 0;
         unsigned int value = 0;
         double dvalue = 0.0;
-        char flag = 0;
-        if (argv[i][j] == '-') {
-            ++j;
-            switch (argv[i][j]) {
-                case 'n':
-                case 'p':
-                case 'h':
-                case 'w':
-                case 'o':
-                case 'm':
-                case 'r':
-                case 'd':
-                case 'e':
-                    flag = argv[i][j];
-                    ++i;
-                    value =
-                        static_cast<unsigned int>(strtol(argv[i], NULL, 10));
-                    if (value <= 0 && flag != 'e') {
-                        error = true;
-                    } else {
-                        switch (flag) {
-                            case 'n':
-                                args.time_steps = value;
-                                break;
-                            case 'p':
-                                args.num_people = value;
-                                break;
-                            case 'h':
-                                args.work_hours_daily = value;
-                                break;
-                            case 'w':
-                                args.work_days_weekly = value;
-                                break;
-                            case 'o':
-                                args.num_products = value;
-                                break;
-                             case 'm':
-                                args.products_per_machine = value;
-                                break;
-                            case 'r':
-                                args.num_producers = value;
-                                break;
-                            case 'd':
-                                args.num_distributors = value;
-                                break;
-                            case 'e':
-                                args.seed = value;
-                                args.fixed_seed = true;
-                                break;
-                            default:
-                                error = true;
-                                break;
-                        }
-                    }
-                    break;
+        std::string arg = argv[i];
 
-                case 's':
-                    ++i;
-                    dvalue = strtod(argv[i], NULL);
-                    if (dvalue <= 0.0 || dvalue >= 1.0) {
-                        error = true;
-                    } else {
-                        args.sickness_chance = dvalue;
-                    }
-                    break;
-                case 'v':
-                    ++i;
-                    dvalue = strtod(argv[i], NULL);
-                    if (dvalue <= 0.0) {
-                        error = true;
-                    } else {
-                        args.ability_stddev = dvalue;
-                    }
-                    break;
+        if(arg == "-j" || arg == "--json") { //"j" needs no additional value
+            args.json = true;
+            continue;
+        }
+        if (i + 1 >= argc) {
+            error = true;
+            break;
+        }
+        
+        if(arg == "-n" || arg == "--time-steps" ||
+           arg == "-p" || arg == "--people" || 
+           arg == "-h" || arg == "--work-hours" || 
+           arg == "-w" || arg == "--work-days" || 
+           arg == "-o" || arg == "--products" || 
+           arg == "-m" || arg == "--products-per-machine" || 
+           arg == "-r" || arg == "--producers" || 
+           arg == "-d" || arg == "--distributors") {
+            value =
+                static_cast<unsigned int>(strtol(argv[++i], NULL, 10));
 
-                case 'j':
-                    args.json = true;
-                    break;
-                default:
+            if (value <= 0) {
                     error = true;
-                    break;
-
+                } 
+            else {
+                if (arg == "-n" || arg == "--time-steps") args.time_steps = value;
+                else if (arg == "-p" || arg == "--people") args.num_people = value;
+                else if (arg == "-h" || arg == "--work-hours") args.work_hours_daily = value;
+                else if (arg == "-w" || arg == "--work-days") args.work_days_weekly = value;
+                else if (arg == "-o" || arg == "--products") args.num_products = value;
+                else if (arg == "-m" || arg == "--products-per-machine") args.products_per_machine = value;
+                else if (arg == "-r" || arg == "--producers") args.num_producers = value;
+                else if (arg == "-d" || arg == "--distributors") args.num_distributors = value;
             }
-            if (error) {
-                break;
+        }
+        else if(arg == "-s" || arg == "--sick-chance") {
+            dvalue = strtod(argv[++i], NULL);
+            if (dvalue <= 0.0 || dvalue >= 1.0) {
+                error = true;
+            } else {
+                args.sickness_chance = dvalue;
+            }
+        }
+        else if(arg == "-v" || arg == "--ability-stddev") {
+            dvalue = strtod(argv[++i], NULL);
+            if (dvalue <= 0.0) {
+                error = true;
+            } else {
+                args.ability_stddev = dvalue;
             }
         } else {
             error = true;
             break;
         }
     }
-    if (error) {
+    if(error) {
         print_usage();
+        exit(1);
     }
 }
 
